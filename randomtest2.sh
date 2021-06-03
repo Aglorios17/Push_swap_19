8761aee88a47817681af78e53a8f470f0512f8dd
Aglorios17 <alessio.glorioso17@gmail.com>
2021-06-03 13:40:22 +0200

    test2

---
+++ randomtest2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+i=0
+TAB=()
+max=100
+while ((i<$max))
+do
+	tester=1
+	RAND=$((1 + RANDOM % 3000))
+	for j in "${TAB[@]}"; do
+		if [[ $j == $RAND ]]
+		then
+			tester=0
+		fi
+	done
+	if [[ $tester == 1 ]]
+	then
+		TAB[i]=$RAND
+		((i+=1))
+	fi
+done
+echo /////////VALUES////////
+echo ${TAB[*]}
+echo ${TAB[*]} > .values
+echo ////////////////////////
+ARG=$(cat .values);
+echo "moves et checker mac :"
+./push_swap $ARG | wc -l
+./push_swap $ARG | ./checker_Mac $ARG
+echo "checker :"
+./push_swap $ARG | ./checker $ARG
+
